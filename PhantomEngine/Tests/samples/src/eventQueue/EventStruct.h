0fb667721578306d6594891c9d744b6088618ba1
李佳 <liphantomjia@gmail.com>
2019-06-29 18:55:56 +0800

    event manager  singleton.
    Signed-off-by: 李佳 <liphantomjia@gmail.com>

---
+++ PhantomEngine/Tests/samples/src/eventQueue/EventStruct.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include <list>
+#include <map>
+
+
+//be harmony
+namespace Phantom {
+	namespace EventQueue{
+
+	enum EventId
+	{
+		EVT_TICK = 0,
+	};
+	struct Event
+	{
+		Event(int $id) : id($id)
+		{}
+		~Event() {}
+		int id;
+	};
+
+	class EventHandler
+	{
+	public:
+		virtual int HandleEvent(Event *evt)
+		{
+		   return 0;
+		 }
+	};
+
+	template < typename Type>
+	class ClassBasedEventHandler :public EventHandler
+	{
+	public :
+		typedef int(Type::* HandlerFunc) (Event *evet);
+	public:
+		ClassBasedEventHandler(Type *ptr, HandlerFunc func) :m_ptr(ptr), m_func(func) {};
+		virtual int HandleEvent(Event *evt)
+		{
+			if (m_ptr && m_func)
+				return (m_ptr->*m_func)(evt);
+			return 0;
+		}
+	private:
+		Type * m_ptr;
+		HandlerFunc m_func;
+	};
+}}
---
+++ PhantomEngine/Tests/samples/src/main.cpp
@@ -0,0 +1,51 @@
+#include "eventQueue/EventManager.h"
+#include "eventQueue/EventManager.h"
+
+using namespace Phantom::EventQueue;
+struct TickEvent : Event
+{
+    TickEvent() : Event(EVT_TICK), time_(0)
+    {}
+   ~TickEvent()
+     {}
+    time_t time_;
+ };
+class UIClass
+{
+public:
+     UIClass()
+     {
+     }
+
+    ~UIClass()
+     {
+     }
+
+     int OnEvent(Event *evt)
+     {
+         TickEvent *tickEvt = (TickEvent*)evt;
+         printf("uiclass event, time now: %d", (int)tickEvt->time_);
+         return 0;
+     }
+};
+int main()
+{
+	EventHandler *eh = new EventHandler();
+	auto& em = EventManager::Instance();
+	em.AddEventHandler(0,eh );
+	em.AddEventHandler(0,eh );
+	em.AddEventHandler(0,eh );
+	em.AddEventHandler(1,eh );
+	em.AddEventHandler(2,eh );
+
+	Event evt(0);
+	em.DispatchEvent(&evt);
+
+	TickEvent tickEvt;
+	UIClass ui;
+	ClassBasedEventHandler<UIClass> evthandler(&ui, &UIClass::OnEvent);
+	em.AddEventHandler(EventId::EVT_TICK, &evthandler);
+	em.DispatchEvent(&tickEvt);
+	em.RemoveEventHandler(EVT_TICK, &evthandler);
+	return 0;
+} 
